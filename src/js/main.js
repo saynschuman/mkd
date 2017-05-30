ae23672a87167fcfb7287e8a63ba893bb939e1f1
saynschuman <saynschuman@gmail.com>
2017-05-30 18:32:12 +0300

    mobile-header finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,10 @@
 (function($) {
     
-
+	
+	$('.mobile-button').click(function(e){
+		e.preventDefault();
+		$('header.header .bottom-header nav ul').slideToggle(200);
+		$(this).toggleClass('active');
+	})
 
 })(jQuery);
