127f66a644ea8e506b77a2e12d8e3cc06147373c
saynschuman <saynschuman@gmail.com>
2017-06-02 14:23:00 +0300

    schedule page + responsive finished

--- src/js/main.js
+++ src/js/main.js
@@ -15,14 +15,35 @@
 	$('.dir').click(function(e){
 		e.preventDefault();
 	})
+
 	$('.dir').focus(function(){
 		$(this).next().slideToggle(350);
 	})
+
 	$('.dir').blur(function(){
 		$(this).next().slideToggle(350);		
 	})
+
 	$('.sublist li').click(function(){
 		$(this).parent().parent().find('a').text($(this).text())
+	})
+
+	$('#course-1 .course-point').click(function(e){
+		e.preventDefault();
+		var title = $(this).attr('data-title');
+		$('#course-1 .subject').removeClass('active');
+		$('#course-1 .subject[data-id="' + title + '"]').addClass('active');
+		$('#course-1 .course-point').removeClass('active');
+		$(this).addClass('active');
+	})
+
+	$('#course-2 .course-point').click(function(e){
+		e.preventDefault();
+		var title = $(this).attr('data-title');
+		$('#course-2 .subject').removeClass('active');
+		$('#course-2 .subject[data-id="' + title + '"]').addClass('active');
+		$('#course-2 .course-point').removeClass('active');
+		$(this).addClass('active');
 	})
 
 })(jQuery);
